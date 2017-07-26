1af3857d4bcd21a5d7b3700f5dcfb929d693d796
Avinash Reddy <beavinash15@gmail.com>
2017-07-25 23:40:30 -0400

    Full project

---
+++ KivaLoan/KivaLoanTableViewCell.swift
@@ -0,0 +1,29 @@
+
+//
+//  KivaLoanTableViewCell.swift
+//  KivaLoan
+//
+//  Created by Avinash on 7/25/2017.
+//  Copyright © 2017 KivaLoan. All rights reserved.
+//
+
+import UIKit
+
+class KivaLoanTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel:UILabel!
+    @IBOutlet weak var countryLabel:UILabel!
+    @IBOutlet weak var useLabel:UILabel!
+    @IBOutlet weak var amountLabel:UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ KivaLoan/Loan.swift
@@ -0,0 +1,16 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Avinash Reddy on 7/25/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Loan {
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
